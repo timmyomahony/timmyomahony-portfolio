8116343660e1699ddca7ef479c18b79de97cebf0
Timmy O'Mahony <hey@timmyomahony.com>
2025-04-14 17:34:42 +0100

    updated submodule to convert new lines in private key

--- bin/init-submodule.sh
+++ bin/init-submodule.sh
@@ -4,7 +4,7 @@ set -e
 
 # SSH setup for GitHub (if needed)
 eval "$(ssh-agent -s)"
-echo "$GIT_SSH_KEY" | tr -d '\r' | ssh-add - > /dev/null
+echo -e "$GIT_SSH_KEY" | tr -d '\r' | ssh-add - > /dev/null
 mkdir -p ~/.ssh
 ssh-keyscan github.com >> ~/.ssh/known_hosts
 
